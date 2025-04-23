5813652ceb482a28b7699f9383b77ab1b364ba8f
Dina Berry <diberry@microsoft.com>
2025-04-23 16:42:17 +0000

    edits and organization

--- widget-js/server/server.ts
+++ widget-js/server/server.ts
@@ -1,5 +1,5 @@
 import { createWidgetServiceRouter } from "../generated/server/src/generated/http/router.js";
-import MyWidgetService from "./widgetService.js";
+import MyWidgetService from "../integration/widgetService.js";
 
 import express from "express";
 import swaggerUi from 'swagger-ui-express';
@@ -12,10 +12,9 @@ import { dirname } from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const thisWidgetService = new MyWidgetService();
-const router = createWidgetServiceRouter(thisWidgetService);
 const app = express();
 
+// Add generated OPENAPI spec
 const swaggerSpecPath = path.resolve(__dirname, '../../generated/spec/openapi3/openapi.yaml');
 
 console.log(`Loading swagger spec from ${swaggerSpecPath}`);
@@ -24,6 +23,9 @@ const swaggerDocument = YAML.parse(swaggerSpec)
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
+// Add generated route
+const thisWidgetService = new MyWidgetService();
+const router = createWidgetServiceRouter(thisWidgetService);
 app.use(router.expressMiddleware);
 
 app.listen(8080, () => {
